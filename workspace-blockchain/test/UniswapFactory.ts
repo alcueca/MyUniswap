7ea450cc4112b9e45a2a3d1417750c85f9441356
Alberto Cuesta Cañada <mail@albertocuesta.es>
2019-12-04 22:17:19 +0000

    UniswapFactory tested.

--- workspace-blockchain/test/UniswapFactory.ts
+++ workspace-blockchain/test/UniswapFactory.ts
@@ -7,7 +7,7 @@ const UniswapFactory = artifacts.require('./UniswapFactory.sol') as Truffle.Cont
 should();
 
 /** @test {UniswapFactory} contract */
-contract('UniswapFactory', (accounts) => {
+contract('UniswapFactory - launchExchange', (accounts) => {
     /**
      * @test {UniswapFactory#launchExchange}
      */
@@ -17,5 +17,45 @@ contract('UniswapFactory', (accounts) => {
         const launchEvent = (await uniswapFactory.launchExchange(token.address)).logs[0];
         launchEvent.event.should.be.equal('ExchangeLaunch');
         launchEvent.args.token.should.be.equal(token.address);
+    });
+});
+
+/** @test {UniswapFactory} contract */
+contract('UniswapFactory - view methods', (accounts) => {
+    let token: TestTokenInstance;
+    let uniswapFactory: UniswapFactoryInstance;
+
+    beforeEach(async () => {
+        token = await TestToken.new('TestToken', 'TST', 18);
+        uniswapFactory = await UniswapFactory.new();
+        await uniswapFactory.launchExchange(token.address)
+    });
+
+    // function tokenToExchangeLookup(address _token) public view returns (address payable exchange);
+    // function exchangeToTokenLookup(address _exchange) public view returns (address token);
+
+    /**
+     * @test {UniswapFactory#getExchangeCount}
+     */
+    it('getExchangeCount.', async () => {
+        (await uniswapFactory.getExchangeCount()).toNumber().should.be.equal(1);
+    });
+
+    /**
+     * @test {UniswapFactory#tokenToExchangeLookup} and {UniswapFactory#exchangeToTokenLookup}
+     */
+    it('address lookups.', async () => {
+        const emptyAddress = '0x0000000000000000000000000000000000000000';
+        const mockAddress = '0x0000000000000000000000000000000000000001';
+
+        const exchangeAddress = (await uniswapFactory.tokenToExchangeLookup(token.address));
+        exchangeAddress.should.be.not.equal(emptyAddress);
+        const tokenAddress = (await uniswapFactory.exchangeToTokenLookup(exchangeAddress));
+        tokenAddress.should.be.equal(token.address);
+
+        const noExchangeAddress = (await uniswapFactory.tokenToExchangeLookup(mockAddress));
+        noExchangeAddress.should.be.equal(emptyAddress);
+        const noTokenAddress = (await uniswapFactory.exchangeToTokenLookup(mockAddress));
+        noTokenAddress.should.be.equal(emptyAddress);
     });
 });
